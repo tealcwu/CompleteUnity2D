41c462d45ece6724daffd53d26dd31f606797d51
Tealc Wu <tealcwu@hotmail.com>
2018-06-06 15:14:43 +0800

    LaserDefender: Add playercontroller script.

---
+++ LaserDefender/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public int MaxLevels = 3;
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("I want quit.");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        //SceneManager.LoadScene(3);
+
+        //Scene scene = SceneManager.GetActiveScene();
+        //string sceneName = scene.name;
+        //int sceneIndex = int.Parse(sceneName.Split('_')[1]);
+
+        //if (sceneIndex < MaxLevels)
+        //{
+        //    SceneManager.LoadScene(string.Format("Level_{0:D2}",sceneIndex + 1));
+        //}
+        //else
+        //{
+        //    SceneManager.LoadScene("Info");
+        //}
+    }
+
+    public void BrickDestroyed()
+    {
+            LoadNextLevel();
+    }
+}
---
+++ LaserDefender/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody2D rb2d;
+
+    public float speed = 8;
+	// Use this for initialization
+	void Start () {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            rb2d.transform.Translate(new Vector3(0f, speed * Time.deltaTime,0f));
+        }
+
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            rb2d.transform.Translate(new Vector3(0f, -speed * Time.deltaTime, 0f));
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb2d.transform.Translate(new Vector3(-speed * Time.deltaTime,0f, 0f));
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb2d.transform.Translate(new Vector3(speed * Time.deltaTime, 0f, 0f));
+        }
+    }
+}
